4b42b8b1729edcbc832028f7cf01281f883527b5
fitdticorp4 <fitditcorp@gmail.com>
2022-06-20 15:24:21 +0900

    First lesson

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print('I"said" do not touch this.')
+print("I'd much rather you 'not'.")
+#This is comment
+print("Now I have started learning python officially")
