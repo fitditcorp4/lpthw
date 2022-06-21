fe1e3c1912ada229155490cbca47593e81682ae9
fitdticorp4 <fitditcorp@gmail.com>
2022-06-21 09:34:02 +0900

    ex5

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Nkem C. Omede'
+my_age = 43 # This is for real
+my_height = 178 #centimeters
+my_weight = 75 #kg
+my_eyes = 'brown'
+my_teeth = 'white'
+my_hair = 'black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} cm tall.")
+print(f"He's {my_weight} kg heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+#This line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"if I add {my_age}, {my_height}, and {my_weight} I get {total}.")
