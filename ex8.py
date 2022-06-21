3616f3b5c558b4199379af7e25f06dddab89b79e
fitdticorp4 <fitditcorp@gmail.com>
2022-06-21 11:40:48 +0900

    ex8

---
+++ ex8.py
@@ -0,0 +1,20 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+"I will like",
+"to become",
+"an expert",
+"in python programming"
+))
+print("What is all this about?{}".format('I will get to see'))
+print(formatter.format(
+"I will like",
+"to become",
+"an expert",
+"!"
+
+))
